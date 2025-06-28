f919291742df8c80fc9181910b0d7d0c209e35ca
RietzF <fredbanksdurietz@gmail.com>
2025-06-27 23:08:07 -0700

    B: added Player object/movement

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill((0, 0, 0))
+        player1.draw(screen)
+        
         pygame.display.flip()
-          
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        dt = clock.tick(60) / 1000
+        player1.update(dt)
+       
+        
 
 if __name__ == "__main__":
         main()
