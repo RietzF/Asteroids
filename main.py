ce3f732ee8d2916e879f62058f19c11a53d6e400
RietzF <the.primeagen@aol.comgit config --list --local>
2025-10-09 16:52:48 -0700

    Draw player and movement

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
+from player import *
 from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        player1.update(dt)
+        
+        screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
